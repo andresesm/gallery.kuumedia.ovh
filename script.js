4679b5ec93980d974cb6197a0dc8e33d9e653042
andresesm <andresesm2@gmail.com>
2025-02-10 17:36:05 -0300

    7th

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     var grid = document.querySelector('.gallery');
 
-    // Iniciar Masonry después de que todas las imágenes se hayan cargado
-    imagesLoaded(grid, function () {
-        new Masonry(grid, {
-            itemSelector: '.gallery-item',
-            columnWidth: '.gallery-sizer',
-            percentPosition: true
+    if (grid) {
+        imagesLoaded(grid, function () {
+            new Masonry(grid, {
+                itemSelector: '.gallery-item',
+                columnWidth: '.gallery-sizer',
+                percentPosition: true
+            });
         });
-    });
+    }
 });
