cae15ad58e488cc0df93f11cefaf81ed2dfe37b8
andresesm <andresesm2@gmail.com>
2025-02-10 18:01:03 -0300

    10th

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var grid = document.querySelector('.grid');
+    var grid = document.querySelector('.masonry');
 
     if (grid) {
         imagesLoaded(grid, function () {
             new Masonry(grid, {
-                itemSelector: '.gallery-item',
-                columnWidth: '.gallery-item',
+                itemSelector: '.masonry-item',
+                columnWidth: '.masonry-item',
                 percentPosition: true
             });
+        });
+    }
+});
+
+// Recalcular Masonry cuando el usuario cambia el tamaño de la ventana
+window.addEventListener('resize', function () {
+    var grid = document.querySelector('.masonry');
+    if (grid) {
+        new Masonry(grid, {
+            itemSelector: '.masonry-item',
+            columnWidth: '.masonry-item',
+            percentPosition: true
         });
     }
 });
