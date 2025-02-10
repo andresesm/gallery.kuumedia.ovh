0891b549b1093f75a5ae0f50c11fe284b0e08204
andresesm <andresesm2@gmail.com>
2025-02-10 17:51:30 -0300

    8th

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var grid = document.querySelector('.gallery');
+    var grid = document.querySelector('.grid');
 
     if (grid) {
         imagesLoaded(grid, function () {
             new Masonry(grid, {
                 itemSelector: '.gallery-item',
-                columnWidth: '.gallery-sizer',
+                columnWidth: '.gallery-item',
                 percentPosition: true
             });
         });
